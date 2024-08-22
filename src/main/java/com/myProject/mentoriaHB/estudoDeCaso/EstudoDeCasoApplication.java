88c6b520ae5db2ee62f162ff00415090ea1df34b
Edfram-Guerra <edfram.guerra@gmail.com>
2024-08-22 09:35:28 -0300

    Projeto inicializado

---
+++ src/main/java/com/myProject/mentoriaHB/estudoDeCaso/EstudoDeCasoApplication.java
@@ -0,0 +1,13 @@
+package com.myProject.mentoriaHB.estudoDeCaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoDeCasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoDeCasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myProject/mentoriaHB/estudoDeCaso/EstudoDeCasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myProject.mentoriaHB.estudoDeCaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoDeCasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
